e956f5aa53759603a1be7155f4b73f929a717f46
Rana.ismael <rana4ismael@gmail.com>
2022-03-30 15:29:37 -0400

    initialising

---
+++ src/main/java/com/project3/revtech/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project3.revtech.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import com.project3.revtech.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	Optional<UserEntity> findByUsername(String username);
+
+	  Boolean existsByUsername(String username);
+
+	  Boolean existsByEmail(String email);
+}
